c339b57edd3b242219f4609d1c1d2ee7d448402a
Diego Marques <dlm352@cornell.edu>
2024-06-24 10:58:32 -0400

    vercel backend add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,10 @@ app.use("/users", userRouter);
 app.use("/conversations", conversationRouter);
 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
